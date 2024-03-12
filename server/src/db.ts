35585c6610a11ef9e8c39444f62ad990b8329c26
DanielZ1337 <danielz2nd@hotmail.com>
2024-03-12 14:46:45 +0100

    updated environment variable

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,14 +1,35 @@
-import {drizzle} from "drizzle-orm/node-postgres";
-import {Client} from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Client } from "pg";
 import * as schema from '../../db/src/schema.ts';
 
-const {DB_URL} = process.env
+const { DB_URL, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+let client
+
 if (!DB_URL) {
-    throw new Error('No url');
+
+    if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME) {
+        throw new Error('Missing environment variables for database connection');
+    }
+
+    client = new Client({
+        host: DB_HOST,
+        port: Number(DB_PORT),
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME
+    });
 }
-const client = new Client({
-    connectionString: process.env.DB_URL,
-});
+
+if (DB_URL) {
+    client = new Client({
+        connectionString: DB_URL
+    });
+}
+
+if (!client) {
+    throw new Error('Could not create database client');
+}
 
 await client.connect();
 
