b64686fc167a8f10adc3b501328d1556d5d4b841
DanielZ1337 <danielz2nd@hotmail.com>
2024-05-31 17:45:30 +0200

    yes

--- db/src/schema.ts
+++ db/src/schema.ts
@@ -1,15 +1,15 @@
-import {type InferSelectModel} from 'drizzle-orm';
-import {decimal, pgTable, serial, text, timestamp,} from 'drizzle-orm/pg-core';
+import { type InferSelectModel } from 'drizzle-orm'
+import { decimal, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
 
 export const trades = pgTable('trades', {
-    id: serial('id').primaryKey(),
-    c: text('c'),
-    p: decimal('p').notNull(),
-    s: text('s').notNull(),
-    t: timestamp('t').notNull(),
-    v: decimal('v').notNull(),
-    createdAt: timestamp('created_at').defaultNow().notNull(),
-    updatedAt: timestamp('updated_at').defaultNow().notNull(),
-});
+	id: serial('id').primaryKey(),
+	c: text('c'),
+	p: decimal('p').notNull(),
+	s: text('s').notNull(),
+	t: timestamp('t').notNull(),
+	v: decimal('v').notNull(),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').defaultNow().notNull(),
+})
 
-export type TradeDatabase = InferSelectModel<typeof trades>;
+export type TradeDatabase = InferSelectModel<typeof trades>
