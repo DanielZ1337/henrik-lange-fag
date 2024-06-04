cd93d598029919044273a0a1f476aeb2603b95fc
DanielZ1337 <danielz2nd@hotmail.com>
2024-06-05 01:57:01 +0200

    minor changes to singleton of drizzle db

--- db/src/drizzle.ts
+++ db/src/drizzle.ts
@@ -1,31 +1,33 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres'
 import { Client } from 'pg'
 import * as schema from './schema.ts'
 
 class Database {
 	private static instance: Database
-	private readonly client: Client
+	private readonly client: NodePgDatabase<typeof schema>
 
 	private constructor() {
 		const { DB_URL, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
 
 		if (DB_URL) {
-			this.client = new Client({ connectionString: DB_URL })
+			const client = new Client({ connectionString: DB_URL })
+			client.connect()
+			this.client = drizzle(client, { schema })
 		} else {
 			if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME) {
 				throw new Error('Missing environment variables for database connection')
 			}
-
-			this.client = new Client({
+			const client = new Client({
 				host: DB_HOST,
 				port: Number(DB_PORT),
 				user: DB_USER,
 				password: DB_PASSWORD,
 				database: DB_NAME,
 			})
-		}
+			client.connect()
 
-		void this.client.connect()
+			this.client = drizzle(client, { schema })
+		}
 	}
 
 	public static getInstance(): Database {
@@ -35,10 +37,8 @@ class Database {
 		return Database.instance
 	}
 
-	public getDb() {
-		return drizzle(this.client, {
-			schema,
-		})
+	public getDb(): NodePgDatabase<typeof schema> {
+		return Database.instance.client
 	}
 }
 
